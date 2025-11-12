3ee6b44d9cbcdea7b13698b0f20a63e38485bcd4
sumi-kml <sumeyakemal04@gmail.com>
2025-11-12 16:48:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,32 @@ import json
 with open('data.json', 'r') as file:
     data = json.load(file)
 
-r1 = data['turrets']['1']['r']
+# Team #: 5
+
+# Our position
+t_r = data['turrets']['1']['r']
+t_theta = data['theta']['1']['theta']
+
+# Passive target positions
+p_r1 = data['globes']['1']['r']
+p_theta1 = data['globes']['1']['theta']
+p_z1 = data['globes']['1']['z']
+p_r2 = data['globes']['2']['r']
+p_theta2 = data['globes']['2']['theta']
+p_z2 = data['globes']['2']['z']
+p_r3 = data['globes']['3']['r']
+p_theta3 = data['globes']['3']['theta']
+p_z3 = data['globes']['3']['z']
 
 # Step 4: Print the extracted data
-print(f"radius: {r1}")
+print(f"turret radius: {t_r}")
+print(f"turret theta: {t_theta}")
+print(f"target 1 r: {p_r1}")
+print(f"target 1 theta: {p_theta1}")
+print(f"target 1 z: {p_z1}")
+print(f"target 2 r: {p_r2}")
+print(f"target 2 theta: {p_theta2}")
+print(f"target 2 z: {p_z2}")
+print(f"target 3 r: {p_r3}")
+print(f"target 3 theta: {p_theta3}")
+print(f"target 3 z: {p_z3}")
