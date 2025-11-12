93b5c5fee137948ed7cda1a30494a8d4b94800ec
sumi-kml <sumeyakemal04@gmail.com>
2025-11-12 16:49:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ t_r = data['turrets']['1']['r']
 t_theta = data['turrets']['1']['theta']
 
 # Passive target positions
-p_r1 = data['globes']['1']['r']
-p_theta1 = data['globes']['1']['theta']
-p_z1 = data['globes']['1']['z']
-p_r2 = data['globes']['2']['r']
-p_theta2 = data['globes']['2']['theta']
-p_z2 = data['globes']['2']['z']
-p_r3 = data['globes']['3']['r']
-p_theta3 = data['globes']['3']['theta']
-p_z3 = data['globes']['3']['z']
+p_r1 = data['globes']['r']
+p_theta1 = data['globes']['theta']
+p_z1 = data['globes']['z']
+p_r2 = data['globes']['r']
+p_theta2 = data['globes']['theta']
+p_z2 = data['globes']['z']
+p_r3 = data['globes']['r']
+p_theta3 = data['globes']['theta']
+p_z3 = data['globes']['z']
 
 # Step 4: Print the extracted data
 print(f"turret radius: {t_r}")
