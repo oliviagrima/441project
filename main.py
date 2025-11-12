18d7a3571312d0fe83493c2d7fddd21f80bd0c51
sumi-kml <sumeyakemal04@gmail.com>
2025-11-12 16:22:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import json
 with open('data.json', 'r') as file:
     data = json.load(file)
 
-r1 = data['turrets']['r']
+r1 = data['turrets']['1']['r']
 
 # Step 4: Print the extracted data
 print(f"radius: {r1}")
