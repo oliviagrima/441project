5c995cc21c3a17d03bff6dcfeacf38ab74932dc2
oliviagrima <100474858@alumnos.uc3m.es>
2025-11-17 15:52:21 -0500

    changes

--- main.py
+++ main.py
@@ -13,21 +13,20 @@ t_theta = data['turrets']['1']['theta']
 
 # Passive target positions
 p_r1 = data['globes'][0]['r']
-"""
-p_theta1 = data['globes']['theta']
-p_z1 = data['globes']['z']
-p_r2 = data['globes']['r']
-p_theta2 = data['globes']['theta']
-p_z2 = data['globes']['z']
-p_r3 = data['globes']['r']
-p_theta3 = data['globes']['theta']
-p_z3 = data['globes']['z']
-"""
+p_theta1 = data['globes'][0]['theta']
+p_z1 = data['globes'][0]['z']
+p_r2 = data['globes'][1]['r']
+p_theta2 = data['globes'][1]['theta']
+p_z2 = data['globes'][1]['z']
+p_r3 = data['globes'][2]['r']
+p_theta3 = data['globes'][2]['theta']
+p_z3 = data['globes'][2]['z']
+
 # Step 4: Print the extracted data
 print(f"turret radius: {t_r}")
 print(f"turret theta: {t_theta}")
 print(f"target 1 r: {p_r1}")
-"""
+
 print(f"target 1 theta: {p_theta1}")
 print(f"target 1 z: {p_z1}")
 print(f"target 2 r: {p_r2}")
@@ -36,4 +35,3 @@ print(f"target 2 z: {p_z2}")
 print(f"target 3 r: {p_r3}")
 print(f"target 3 theta: {p_theta3}")
 print(f"target 3 z: {p_z3}")
-"""
