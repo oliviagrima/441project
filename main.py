ab8e9e5eab4b9ff38b4f0d4d99e6c3afa2b2611c
oliviagrima <100474858@alumnos.uc3m.es>
2025-11-19 16:09:22 -0500

    main changes

--- main.py
+++ main.py
@@ -69,39 +69,3 @@ def read_targets():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-"""
-with open('data.json', 'r') as file:
-    data = json.load(file)
-
-
-# Team #: 5
-# Our position
-t_r = data['turrets']['1']['r']
-t_theta = data['turrets']['1']['theta']
-
-# Passive target positions
-p_r1 = data['globes'][0]['r']
-p_theta1 = data['globes'][0]['theta']
-p_z1 = data['globes'][0]['z']
-p_r2 = data['globes'][1]['r']
-p_theta2 = data['globes'][1]['theta']
-p_z2 = data['globes'][1]['z']
-p_r3 = data['globes'][2]['r']
-p_theta3 = data['globes'][2]['theta']
-p_z3 = data['globes'][2]['z']
-
-# Step 4: Print the extracted data
-print(f"turret radius: {t_r}")
-print(f"turret theta: {t_theta}")
-print(f"target 1 r: {p_r1}")
-
-print(f"target 1 theta: {p_theta1}")
-print(f"target 1 z: {p_z1}")
-print(f"target 2 r: {p_r2}")
-print(f"target 2 theta: {p_theta2}")
-print(f"target 2 z: {p_z2}")
-print(f"target 3 r: {p_r3}")
-print(f"target 3 theta: {p_theta3}")
-print(f"target 3 z: {p_z3}")
-"""
